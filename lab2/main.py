16c2f062db2834461607c3dca65fa29a8998857f
puzzzik <vtazenkovv@yandex.ru>
2021-09-29 18:42:34 +0300

    a

--- lab2/main.py
+++ lab2/main.py
@@ -1,7 +1,19 @@
-import circle.py
+from lab_python_oop.rectangle import Rectangle
+from lab_python_oop.circle import Circle
+from lab_python_oop.square import Square
 
 
+def main():
+    r = Rectangle("синего", 3, 2)
+    c = Circle("зеленого", 5)
+    s = Square("красного", 5)
+    print(r)
+    print(c)
+    print(s)
 
+
+def main():
+    pass
 
 if __name__ == '__main__':
     main()
