2f3fd906a022ee5c2d5db8eefff30a2c14d839db
puzzzik <vtazenkovv@yandex.ru>
2021-12-21 20:22:46 +0300

    Sema lab6

--- Sem lab6/keyboards.py
+++ Sem lab6/keyboards.py
@@ -27,16 +27,16 @@ kindergarten.add(kindergarten_btn_1, kindergarten_btn_2)
 school = ReplyKeyboardMarkup(row_width=1, one_time_keyboard=False, resize_keyboard=True).add(
     KeyboardButton(text='пойти в школу'))
 
-uni = ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True, resize_keyboard=True)
+uni = ReplyKeyboardMarkup(row_width=2, one_time_keyboard=False, resize_keyboard=True)
 uni1 = KeyboardButton(text='идти в университет')
 uni2 = KeyboardButton(text='пойду в колледж')
 uni.add(uni1, uni2)
 
-work = ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True, resize_keyboard=True)
+work = ReplyKeyboardMarkup(row_width=2, one_time_keyboard=False, resize_keyboard=True)
 work1 = KeyboardButton(text="на работку :(")
 
 work2 = KeyboardButton(text='нет, умереть.')
 work.add(work1, work2)
 
 diebtn = KeyboardButton(text='умереть.')
-die = ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True, resize_keyboard=True).add(diebtn)
+die = ReplyKeyboardMarkup(row_width=1, one_time_keyboard=False, resize_keyboard=True).add(diebtn)
